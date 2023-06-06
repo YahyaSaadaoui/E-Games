e83739f2ad18870e585722c1f03ad1ec57081eef
Yahya saadaoui <99345333+YahyaSaadaoui@users.noreply.github.com>
2023-06-06 01:14:23 +0100

    Update settings.py

--- egames/settings.py
+++ egames/settings.py
@@ -109,6 +109,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 

05fdcdd58f4885dc942b92514dd9a46daaffe190
Yahya saadaoui <99345333+YahyaSaadaoui@users.noreply.github.com>
2023-06-06 01:14:55 +0100

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -18,3 +18,5 @@ urlpatterns = [
     path('e_news/', views.e_news, name='e_news'),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
